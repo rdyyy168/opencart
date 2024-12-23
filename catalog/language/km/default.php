273e2fc3ea6bd83c0b27d61708511ef10eb0eae0
rdyyy168 <doeunrady3@gmail.com>
2024-12-23 17:30:52 +0700

    first commit

---
+++ catalog/language/km/default.php
@@ -0,0 +1,23 @@
+<?php
+// Locale
+$_['code']                  = 'km';
+$_['direction']             = 'ltr';
+$_['date_format_short']     = 'd/m/Y';
+$_['date_format_long']      = 'l jS F Y';
+$_['time_format']           = 'h:i:s A';
+$_['datetime_format']       = 'd/m/Y H:i:s';
+$_['decimal_point']         = '.';
+$_['thousand_point']        = ',';
+?>
+<?php
+// Text
+$_['text_home']         = '<i class="fas fa-home"></i>';
+$_['text_yes']          = 'បាទ/ចាស';
+$_['text_no']           = 'ទេ';
+$_['text_none']         = ' --- គ្មាន --- ';
+$_['text_select']       = ' --- សូមជ្រើសរើស --- ';
+$_['text_all_zones']    = 'គ្រប់តំបន់';
+$_['text_pagination']   = 'កំពុងបង្ហាញ %d ដល់ %d នៃ %d (%d ទំព័រ)';
+$_['text_loading']      = 'កំពុងផ្ទុក...';
+$_['text_no_results']   = 'គ្មានលទ្ធផល!';
+?>
